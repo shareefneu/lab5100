2ec5fa0733b83d0db654a74b27decd8df3deeeb9
shareefneu <145611175+shareefneu@users.noreply.github.com>
2023-11-27 23:16:44 -0500

    Facade Design Pattern
    Facade Design Pattern

---
+++ Assignment_DesignPatterns/FacadeDesignPatternDemo/src/facadedesignpatternsdemo/Shape.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package facadedesignpatternsdemo;
+
+/**
+ * This is a Shape Interface class.
+ * It contains one method draw().
+ * Each classes which implement the Shape interface must have their own definition of the draw() method.
+ * @author amer
+ * @version 1.0
+ */
+public interface Shape {
+    String draw();
+}
